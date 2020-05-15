212e5902cb12086fac26dc7ba7003e06673aee2c
Ross Bolotin <rossbolotin@mail.tau.ac.il>
2020-05-15 16:15:32 +0300

    mnist loaded

--- ex4.py
+++ ex4.py
@@ -11,10 +11,10 @@ y = mnist['target']
 random_state = check_random_state(0)
 permutation = random_state.permutation(X.shape[0])
 X = X[permutation]
-y = y[permutation] # The next line flattens the vector into 1D array of size 784
+y = y[permutation]  # The next line flattens the vector into 1D array of size 784
 X = X.reshape((X.shape[0], -1))
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
- scaler = StandardScaler() # the next lines standardize the images
+scaler = StandardScaler() # the next lines standardize the images
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
